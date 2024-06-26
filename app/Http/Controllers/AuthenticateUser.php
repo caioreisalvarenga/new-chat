f4766c38f53fd73700c5d7c343e7cbb0bc599be2
Caio Alvarenga <caioreisalvarenga@gmail.com>
2024-06-26 19:38:07 -0300

    profileId, profileAuth methods authenticate sanctum

--- app/Http/Controllers/AuthenticateUser.php
+++ app/Http/Controllers/AuthenticateUser.php
@@ -46,4 +46,63 @@ class AuthenticateUser extends Controller
             ], 500);
         }
     }
+
+    public function login(Request $request)
+    {
+        try {
+            $validateUser = Validator::make($request->all(), [
+                'email' => 'required|email',
+                'password' => 'required',
+            ]);
+
+            if ($validateUser->fails()) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'validation error',
+                    'erros' => $validateUser->errors()
+                ], 401);
+            }
+
+            if (!Auth::attempt($request->only(['email', 'password']))) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'EMAIL or PASSWORD are incorrect',
+                ], 401);
+            }
+
+            $user = User::where('email', $request->email)->first();
+            return response()->json([
+                'status' => true,
+                'message' => 'User logged in successfully',
+                'token' => $user->createToken("API TOKEN")->plainTextToken
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage(),
+                'erros' => $validateUser->errors()
+            ], 500);
+        }
+    }
+
+    public function profileIdAuth()
+    {
+        $userData = auth()->user();
+        return response()->json([
+            'status' => true,
+            'message' => 'Profile Information',
+            'data' => $userData,
+            'id' => auth()->user()->id
+        ], 200);
+    }
+
+    public function profileAuth(Request $request)
+    {
+        $userData = auth()->user();
+        return response()->json([
+            'status' => true,
+            'message' => 'Profile Information',
+            'data' => $userData,
+        ], 200);
+    }
 }
