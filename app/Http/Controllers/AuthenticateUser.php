4e8ac9ab0956d2fca952c54bc62bb7a8cdbd663e
Caio Alvarenga <caioreisalvarenga@gmail.com>
2024-06-26 16:32:11 -0300

    register method sanctum controller yet

--- app/Http/Controllers/AuthenticateUser.php
+++ app/Http/Controllers/AuthenticateUser.php
@@ -2,20 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class AuthenticateUser extends Controller
 {
-    public function login(Request $request)
+    public function register(Request $request)
     {
-        $credentials = $request->only('email', 'password');
+        try {
+            $validateUser = Validator::make($request->all(), [
+                'name' => 'required',
+                'email' => 'required|email|unique:users,email',
+                'password' => 'required',
+            ]);
 
-        if (Auth::attempt($credentials)) {
-            $token = $request->user()->createToken('Token Name')->plainTextToken;
-            return response()->json(['token' => $token], 200);
+            if ($validateUser->fails()) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'validation error',
+                    'erros' => $validateUser->errors()
+                ], 401);
+            }
+
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => $request->password
+            ]);
+
+            return response()->json([
+                'status' => true,
+                'message' => 'User created successfully',
+                'token' => $user->createToken("API TOKEN")->plainTextToken
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage(),
+                'erros' => $validateUser->errors()
+            ], 500);
         }
-
-        return response()->json(['error' => 'Credenciais inválidas'], 401);
     }
 }
