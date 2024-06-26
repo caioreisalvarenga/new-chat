c9fb54c1d104680ddfe84670646a0ddf50a34e8b
Caio Alvarenga <caioreisalvarenga@gmail.com>
2024-06-26 16:30:53 -0300

    updating register api route

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\AuthenticateUser;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/register', [AuthenticateUser::class, 'register'])->name('register');
+
